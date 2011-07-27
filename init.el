6d38c14d50d0bf4e67b95b9b6cff4c55cf3abba0
Sean Fisk <sean@seanfisk.com>
2011-07-26 23:44:21 -0400

    Disable cua-mode because it was annoying me.

--- init.el
+++ init.el
@@ -169,7 +169,7 @@
 (add-to-list 'default-frame-alist '(alpha . 100))
 
 ;; copy/paste with C-c and C-v and C-x, check out C-RET too
-(cua-mode)
+;(cua-mode)
 
 ;; Use the clipboard, pretty please, so that copy/paste "works"
 (setq x-select-enable-clipboard t)
