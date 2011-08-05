f973494b86a5521791884cea0f39910889eabac9
Sean Fisk <sean.fisk@atomicobject.com>
2011-08-05 00:31:24 -0400

    Work-around for Emacs 23.1 and auto-indent-mode.

--- init.el
+++ init.el
@@ -77,8 +77,28 @@
    (:name auto-indent-mode		; auto-indentation, should be loaded before autopair and yasnippet
 	  :type emacswiki
 	  :features auto-indent-mode
+ 	  :before (lambda ()
+		    			; Work-around for Emacs 23.1 to compile auto-indent-mode
+                                        ; Please see <http://paste.lisp.org/display/115598>
+		    
+                                        ; If we have a version of called-interactively-p that doesn't accept            
+                                        ; arguments, redefine it so that it does take arguments. This                   
+                                        ; retains compatibility with packages that pass arguments to                    
+                                        ; called-interactively-p.                                                       
+		    
+                    (condition-case nil (called-interactively-p 'interactive)
+                      (wrong-number-of-arguments
+                                        ; Save reference to called-interactively-p in                                
+                                        ; substitute-called-interactively-p                                    
+                       (fset 'substitute-called-interactively-p
+                             (symbol-function 'called-interactively-p))
+                                        ; Define called-interactively-p so that it discards                          
+                                        ; its arguments and calls substitute-called-interactively-p            
+                       (fset 'called-interactively-p
+                             (lambda (&rest args)
+                               (substitute-called-interactively-p))))))
 	  :after (lambda ()
-		   (auto-indent-global-mode t)))
+	   	   (auto-indent-global-mode t)))
    (:name autopair			; automatically complete everything that comes in pairs, load auto-indent-mode first
 	  :depends auto-indent-mode
 	  :after (lambda ()
