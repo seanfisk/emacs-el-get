54f206212ee3a42e8263f13bff8171bc2ce81e86
Sean Fisk <sean@seanfisk.com>
2011-09-07 01:30:43 -0400

    Styling for C-like modes.

--- init.el
+++ init.el
@@ -444,3 +444,12 @@
   (setq autopair-dont-activate t))
 
 (add-hook 'term-mode-hook 'term-custom)
+
+;;; c-like modes
+(defun c-mode-common-custom ()
+  (setq c-default-style "bsd")		; BSD (Allman) style indentation
+  (setq c-basic-offset 2)		; 2-space tab size
+  (setq indent-tabs-mode t)		; use tabs
+  (setq tab-width 2))			; small tabs
+
+(add-hook 'c-mode-common-hook 'c-mode-common-custom)
