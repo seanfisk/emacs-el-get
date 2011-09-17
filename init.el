3ed465beefb91489a96b923d29f697407c8596f0
Sean Fisk <sean@seanfisk.com>
2011-09-16 21:28:09 -0400

    Remove custom crap that somehow got added.

--- init.el
+++ init.el
@@ -487,15 +487,3 @@
   (setq tab-width 2))			; small tabs
 
 (add-hook 'c-mode-common-hook 'c-mode-common-custom)
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(flymake-python-syntax-checker "pep8"))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
