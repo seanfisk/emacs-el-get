24805cd9c8a32b974bd38106c02228c85cfaee54
Sean Fisk <sean@seanfisk.com>
2011-09-27 02:58:36 -0400

    Simplify and correct the install script.

--- install.bash
+++ install.bash
@@ -15,8 +15,10 @@ fi
 readonly SCRIPT_NAME=$(basename "$0")
 readonly INSTALL_DIR=~/.emacs.d
 
-if [[ -d $INSTALL_DIR ]]; then
-	mkdir -p "$INSTALL_DIR"
+if [[ $1 == install && ! -d $INSTALL_DIR ]]; then
+    set -o xtrace
+    mkdir -p "$INSTALL_DIR"
+    set +o xtrace
 fi
 
 for FILE in init.el src; do
@@ -24,42 +26,12 @@ for FILE in init.el src; do
 	SOURCE=$(pwd -P)/$FILE
 	case $1 in
 		install)
-			SKIP_FILE=
-			if [[ -e $DEST || -L $DEST ]]; then
-				VALID=
-				until [[ $VALID ]]; do
-					read -p "File \`$DEST' exists. ([b]ackup, [o]verwrite, [s]kip, [q]uit)? " CHOICE
-					VALID=yes
-					case $CHOICE in
-						b | backup)
-							set -o xtrace
-							mv -i "$DEST" "$DEST.old"
-							set +o xtrace
-							break
-							;;
-						o | overwrite)
-							break
-							;;
-						s | skip)
-							SKIP_FILE=yes
-							break
-							;;
-						q | quit)
-							exit
-							;;
-						*)
-							VALID=
-							;;
-					esac
-				done
-			fi
-			
-			if [[ $SKIP_FILE ]]; then
-				continue
-			fi
-			
 			set -o xtrace
-			ln -sf "$SOURCE" "$INSTALL_DIR"
+			ln --backup=existing \
+			    --interactive \
+			    --symbolic \
+			    --no-target-directory \
+			    "$SOURCE" "$DEST"
 			set +o xtrace
 			;;
 		remove)
