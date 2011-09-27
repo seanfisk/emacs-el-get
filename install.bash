b0dd17e4c76f3b3ad52f1a8790426930a5f75faa
Sean Fisk <sean@seanfisk.com>
2011-09-27 02:22:19 -0400

    Improve install script, add removal option.

--- install.bash
+++ install.bash
@@ -2,53 +2,75 @@
 set -o nounset
 set -o errexit
 
-readonly INSTALL_DIR="$HOME/.emacs.d"
-readonly IGNORE_REGEX='(^\.git(ignore|modules)?|install.bash)|\.markdown$'
+usage()
+{
+	echo "Usage: $0 [install | remove]" 1>&2
+	exit 1
+}
 
-if [[ -d "$INSTALL_DIR" ]]; then
-    echo "\`$INSTALL_DIR' exists. Please remove it before continuing." 1>&2
-    exit 1
+if [[ $# -ne 1 ]]; then
+	usage
 fi
-mkdir "$INSTALL_DIR"
 
-for FILE in $(ls -A | grep -iEv "$IGNORE_REGEX"); do
-	DEST="$INSTALL_DIR/$FILE"
-	SOURCE="$(pwd -P)/$FILE"
-	SKIP_FILE=
-	if [[ -e $DEST || -L $DEST ]]; then
-		VALID=
-		until [[ $VALID ]]; do
-			read -p "File \`$DEST' exists. ([b]ackup, [o]verwrite, [s]kip, [q]uit)? " CHOICE
-			VALID=yes
-			case "$CHOICE" in
-				b | backup)
-					set -o xtrace
-					mv -i "$DEST" "$DEST.old"
-					set +o xtrace
-					break
-					;;
-				o | overwrite)
-					break
-					;;
-				s | skip)
-					SKIP_FILE=yes
-					break
-					;;
-				q | quit)
-					exit
-					;;
-				*)
-					VALID=
-					;;
-			esac
-		done
-	fi
-	
-	if [[ $SKIP_FILE ]]; then
-		continue
-	fi
-	
-	set -o xtrace
-        ln -sf "$SOURCE" "$INSTALL_DIR"
-	set +o xtrace
+readonly SCRIPT_NAME=$(basename "$0")
+readonly INSTALL_DIR=~/.emacs.d
+
+if [[ -d $INSTALL_DIR ]]; then
+	mkdir -p "$INSTALL_DIR"
+fi
+
+for FILE in init.el src; do
+	DEST=$INSTALL_DIR/$FILE
+	SOURCE=$(pwd -P)/$FILE
+	case $1 in
+		install)
+			SKIP_FILE=
+			if [[ -e $DEST || -L $DEST ]]; then
+				VALID=
+				until [[ $VALID ]]; do
+					read -p "File \`$DEST' exists. ([b]ackup, [o]verwrite, [s]kip, [q]uit)? " CHOICE
+					VALID=yes
+					case $CHOICE in
+						b | backup)
+							set -o xtrace
+							mv -i "$DEST" "$DEST.old"
+							set +o xtrace
+							break
+							;;
+						o | overwrite)
+							break
+							;;
+						s | skip)
+							SKIP_FILE=yes
+							break
+							;;
+						q | quit)
+							exit
+							;;
+						*)
+							VALID=
+							;;
+					esac
+				done
+			fi
+			
+			if [[ $SKIP_FILE ]]; then
+				continue
+			fi
+			
+			set -o xtrace
+			ln -sf "$SOURCE" "$INSTALL_DIR"
+			set +o xtrace
+			;;
+		remove)
+			if [[ -L "$DEST" && $(readlink "$DEST") == "$SOURCE" ]]; then
+				set -o xtrace
+				rm "$DEST"
+				set +o xtrace
+			fi
+			;;
+		*)
+			usage
+			;;
+	esac
 done
