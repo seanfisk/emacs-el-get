cc5192471b252907070a8804836b2402cce4567e
Sean Fisk <sean@seanfisk.com>
2011-11-08 14:49:16 -0500

    Fix some python-rope stuff. Enable ropemacs-mode, since it wasn't starting in python-mode by default for some reason.

--- init.el
+++ init.el
@@ -242,16 +242,15 @@
 			 :depends auto-complete ; for the stuff below
 			 :after (lambda ()
 				  ;; for this to work, you must have `rope', `ropemacs', and `ropemode' installed through pip
-                                  ;; redefine this since the `ac-omni-completion-sources' is deprecated
-                                  (defun ac-ropemacs-setup ()
-                                    (ac-ropemacs-require)
-                                    (setq ac-sources (append (list 'ac-source-ropemacs) ac-sources))
-                                    ;;(setq ac-omni-completion-sources '(("\\." ac-source-ropemacs)))
-                                    )
-                                  
+				  (defun python-ropemacs-custom ()
+				    (ropemacs-mode t))
+				  
+				  (add-hook 'python-mode-hook 'python-ropemacs-custom)
+				  
                                   ;; set up auto-complete for ropemacs
 				  ;; this does all the necessary rope setup as well
-                                  (ac-ropemacs-initialize)))
+                                  (ac-ropemacs-initialize)
+				  ))
 		  (:name pythonbrew-mini ; Emacs interface to pythonbrew
 			 :type git
 			 :url "git://github.com/franckcuny/pythonbrew-mini.el.git"
